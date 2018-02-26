f5a24e5a198951687ca346f4d422ce34eb90f1d6
lordprana <matthew.gaba.2@gmail.com>
2018-02-26 17:32:24 -0500

    Added Address table

---
+++ server/db/models/address.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Address = db.define('address', {
+  street: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  city: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  state: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  country: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  isActive: {
+    type: Sequelize.BOOLEAN,
+    allowNull: false
+  }
+})
+
+module.exports = Address;
