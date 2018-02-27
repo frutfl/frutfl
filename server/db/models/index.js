34306f73d43a915b49a7acaaca712cac33498e71
Robert Rinaldo <rinaldo646@gmail.com>
2018-02-27 15:30:28 -0500

    finished creating associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,20 +1,38 @@
 const Address = require('./address');
 const Category = require('./category');
 const Order = require('./order');
-
+const OrderItem = require('./orderItem');
 const Product = require('./product');
-
+const Review  = require('./review');
 const User = require('./user');
 
-User.hasMany(Address);
 Address.belongsTo(User);
+Address.hasMany(Order);
+
+Category.hasMany(Product, {through: 'ProductCategory'});
+
+Order.belongsTo(Address);
+Order.belongsTo(User);
+Order.hasMany(OrderItem);
+
+OrderItem.belongsTo(Order);
+
+Product.hasMany(Category, {through: 'ProductCategory'});
+Product.hasMany(Review);
+
+Review.belongsTo(Product);
+Review.belongsTo(User);
+
+User.hasMany(Address);
+User.hasMany(Order);
+User.hasMany(Review);
 
 module.exports = {
   Address,
   Category,
   Order,
-
+  OrderItem,
   Product,
-
+  Review,
   User,
 };
