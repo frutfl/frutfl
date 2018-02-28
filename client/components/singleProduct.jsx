d8bfa0311d73d5825bf7f5401d291170ffd61755
Whitman Johnson <whit.ross.j@gmail.com>
2018-02-28 12:46:42 -0500

    initialized singleProduct component

---
+++ client/components/singleProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class ProductPage extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = 
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    componentDidMount(){
+
+    }
+}
