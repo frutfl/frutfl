fea07d86c1dfd378f6c022952ad4490403a0d26d
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-01 15:04:15 -0500

    made skeleton of routes

---
+++ server/api/addresses.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const { Address } = require('../db/models');
+module.exports = router;
+
+router.get('/', (req, res, next) => {
+
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+router.put('/:addressId', (req, res, next) => {
+
+});
+
+router.delete('./:addressId', (req, res, next) => {
+
+});
