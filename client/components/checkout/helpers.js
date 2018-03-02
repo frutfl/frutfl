75300cc965482483bbca40a8af9aa1e5e0d4e4f8
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-02 16:29:29 -0500

    integrated zip code

--- client/components/checkout/helpers.js
+++ client/components/checkout/helpers.js
@@ -1,6 +1,7 @@
 export const addressViewableAttributes = [
   {dbCol: 'name', label: 'Full Name', type: 'text', required: true },
   {dbCol: 'street', label: 'Street Address', type: 'text', required: true },
+  {dbCol: 'zipCode', label: 'Zip Code', type: 'text', required: true },
   {dbCol: 'city', label: 'City', type: 'text', required: true },
   {dbCol: 'state', label: 'State', type: 'text', required: true },
   {dbCol: 'country', label: 'Country', type: 'text', required: true }
