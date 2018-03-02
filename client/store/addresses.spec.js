a4ee301578f72fe8086402a32ac29aa7c2c957f9
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-02 16:19:24 -0500

    finished thunk tests

--- client/store/addresses.spec.js
+++ client/store/addresses.spec.js
@@ -17,7 +17,12 @@ describe('thunk creators', () => {
       addressList: [],
       selectedId: null,
     }
-  }
+  };
+
+  const fakeAddresses = [
+    { id: 1, name: 'name1', street: 'street1', city: 'city1', state: 'state1', country: 'country1', isActive: 'true', userId: 1 },
+    { id: 2, name: 'name2', street: 'street2', city: 'city2', state: 'state2', country: 'country2', isActive: 'true', userId: 2 }
+  ];
 
   beforeEach(() => {
     mockAxios = new MockAdapter(axios);
@@ -31,10 +36,6 @@ describe('thunk creators', () => {
 
   describe('fetchAddresses', () => {
     it('dispatches GET_ADDRESSES', () => {
-      const fakeAddresses = [
-        { name: 'name1', street: 'street1', city: 'city1', state: 'state1', country: 'country1', isActive: 'true', userId: 1 },
-        { name: 'name2', street: 'street2', city: 'city2', state: 'state2', country: 'country2', isActive: 'true', userId: 2 }
-      ];
       mockAxios.onGet('/api/addresses').replyOnce(200, fakeAddresses);
       return store.dispatch(fetchAddresses())
         .then(() => {
@@ -44,4 +45,58 @@ describe('thunk creators', () => {
         });
     });
   });
+
+  describe('postAddresses', () => {
+    it('dispatches ADD_ADDRESS', () => {
+      const newAddress = { id: 3, name: 'name3', street: 'street3', city: 'city3', state: 'state3', country: 'country3', isActive: 'true', userId: 3 };
+      mockAxios.onPost('/api/addresses').replyOnce(200, newAddress);
+      return store.dispatch(postAddress())
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0].type).to.be.equal('ADD_ADDRESS');
+          expect(actions[0].address).to.be.deep.equal(newAddress);
+        });
+    });
+  });
+
+  describe('putAddress', () => {
+    it('dispatches EDIT_ADDRESS', () => {
+      const updatedAddress = { id: 4, name: 'name4', street: 'street4', city: 'city4', state: 'state4', country: 'country4', isActive: 'true', userId: 4 };
+      mockAxios.onPut('/api/addresses/4').replyOnce(200, updatedAddress);
+      return store.dispatch(putAddress(updatedAddress))
+        .then(() => {
+          const actions = store.getActions();
+          console.log('ACTIONS ARE:', actions)
+          expect(actions[0].type).to.be.equal('EDIT_ADDRESS');
+          expect(actions[0].address).to.be.deep.equal(updatedAddress);
+        });
+    });
+  });
+
+  describe('deleteAddress', () => {
+    it('dispatches REMOVE_ADDRESS', () => {
+      const toBeDeleted = { id: 5, name: 'name5', street: 'street5', city: 'city5', state: 'state5', country: 'country5', isActive: 'true', userId: 5 };
+      mockAxios.onDelete('/api/addresses/5').replyOnce(204);
+      return store.dispatch(deleteAddress(toBeDeleted))
+        .then(() => {
+          const actions = store.getActions();
+          expect(actions[0].type).to.be.equal('REMOVE_ADDRESS');
+          expect(actions[0].address).to.be.deep.equal(toBeDeleted);
+        });
+    });
+  });
 });
+
+// describe('addresses reducer', () => {
+
+//   const fakeAddresses = [
+//     { id: 1, name: 'name1', street: 'street1', city: 'city1', state: 'state1', country: 'country1', isActive: 'true', userId: 1 },
+//     { id: 2, name: 'name2', street: 'street2', city: 'city2', state: 'state2', country: 'country2', isActive: 'true', userId: 2 }
+//   ];
+
+//   describe('GET_ADDRESSES', () => {
+//     it('returns addresses', () => {
+
+//     })
+//   })
+// })
