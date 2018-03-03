b4516423c5909ab022ab92e7def31bce9b9631ff
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-03 18:16:11 -0500

    basic front-end stripe integration working

---
+++ client/components/checkout/CheckoutForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {injectStripe, CardElement} from 'react-stripe-elements';
+
+class CheckoutForm extends React.Component {
+  handleSubmit = (event) => {
+    // We don't want to let default form submission happen here, which would refresh the page.
+    event.preventDefault();
+    console.log('address id:', this.props.addressId);
+    // Within the context of `Elements`, this call to createToken knows which Element to
+    // tokenize, since there's only one in this group.
+    this.props.stripe.createToken({name: 'Jenny Rosen'}).then(({token}) => {
+      console.log('Received Stripe token:', token);
+    });
+
+    // However, this line of code will do the same thing:
+    //this.props.stripe.createToken({type: 'card', name: 'Jenny Rosen'}).then(result => console.log('result is:', result));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+        Card details
+        <CardElement style={{base: {fontSize: '18px'}}} />
+      </label>
+        <button>Confirm order</button>
+      </form>
+    );
+  }
+}
+
+const mapState = state => ({
+  addressId: state.addresses.selectedId,
+});
+
+export default injectStripe(connect(mapState)(CheckoutForm));
