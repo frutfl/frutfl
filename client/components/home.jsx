5b1cfa63feabd8f4bd95bac611307d867dd7c0d0
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-05 15:01:17 -0500

    feature(home): product search

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -7,21 +7,42 @@ import {fetchAllProducts} from '../store';
 import Categories from './categories.jsx';
 
 class Home extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            search: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
     componentDidMount(){
         this.props.onLoad();
     }
 
+    handleChange(evt){
+        const search = evt.target.value;
+        this.setState({ search });
+    }
+
     render() {
-        const products = ( this.props.category === 'ALL' ) ?
-                         this.props.products :
-                         this.props.products.filter(product => {
+        let products = ( this.props.category === 'ALL' ) ?
+                       this.props.products.filter(product => {
+                           return (product.name.toLowerCase().includes(this.state.search.toLowerCase()));
+                       })
+                     : this.props.products.filter(product => {
                              const match = product.categories.filter(category => {
                                  return category.id === this.props.category;
                              });
-                             return match.length;
+                             return ( match.length && product.name.toLowerCase().includes(this.state.search.toLowerCase()));
                          });
-        return(
+        return (
             <div>
+                <form>
+                    <label>
+                        Search:
+                        <input type="text" value={this.state.search} onChange={this.handleChange} />
+                    </label>
+                </form>
                 <Categories />
                 <Products products={products} />
             </div>
