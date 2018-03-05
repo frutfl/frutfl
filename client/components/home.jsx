80634fdc8d43a4a3abd163666a4133d7d451a88a
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-05 13:00:19 -0500

    removed test products from home component

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -37,33 +37,9 @@ const mapState = state => {
 };
 
 const mapDispatch = dispatch => {
-    // const product1 = {
-    //     "name": "Braeburn Apples",
-    //     "Category1": "Apples",
-    //     "Category2": "test category",
-    //     "species": "Apples",
-    //     "variety": "Braeburn",
-    //     "organic": "FALSE",
-    //     "price": 1.24,
-    //     "unit": "per pound",
-    //     "inventory": 10,
-    //     "isCurrentlyAvailable": "TRUE"
-    //   };
-    //   const product2 = {
-    //     "name": "Limes",
-    //     "Category1": "Citrus",
-    //     "species": "Limes",
-    //     "organic": "FALSE",
-    //     "price": 0.3,
-    //     "unit": "each",
-    //     "inventory": 103,
-    //     "isCurrentlyAvailable": "TRUE"
-    //   };
     return {
         onLoad() {
             dispatch(fetchAllProducts());
-            // dispatch(writeCartItemToStorage(product1, 1));
-            // dispatch(writeCartItemToStorage(product2, 4));
         }
     };
 };
