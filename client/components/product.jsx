2cdfc61de0fc9e10eb5adbd641f7c596148a3eb0
Whitman Johnson <whit.ross.j@gmail.com>
2018-03-06 12:12:37 -0500

    feature(components/product): prices and volumes now display on multi-product view

--- client/components/product.jsx
+++ client/components/product.jsx
@@ -6,11 +6,15 @@ export default function Product(props) {
     const product = props.product;
     const name = product.name;
     const image = product.photos[0];
+    const quantity = product.unit;
+    const price = +product.price;
+    console.log(typeof price)
     return (
         <Link to={`products/${product.id}`}>
             <div>
-                <h4>{name}</h4>
+                <h4>{name} </h4>
                 <img src={image} />
+                <h5>{price.toFixed(2)} USD {quantity}</h5>
             </div>
         </Link>
     );
