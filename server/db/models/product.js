a026fda80123963b5258ed50de6eeb548fb42589
Robert Rinaldo <rinaldo646@gmail.com>
2018-03-06 15:33:35 -0500

    added default description

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -43,6 +43,13 @@ const Product = db.define('product', {
       return (this.getDataValue('organic') ? 'Organic ' : '') + (this.getDataValue('variety') ? this.getDataValue('variety') + ' ' : '') + this.getDataValue('species');
     }
   }
+},
+{
+  hooks: {
+    beforeCreate: product => {
+      product.description = `${product.name} are a delicious fruit. They can be enjoyed while perusing some lorem ipsum text. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`;
+    }
+  }
 });
 
 module.exports = Product;
